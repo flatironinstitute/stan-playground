08ddf1ffcf3d4c94539d4c3aaed43a56bf649d56
Jeremy Magland <jmagland@flatironinstitute.org>
2024-05-17 07:43:45 -0400

    memoize data in SequencePlotWidget

--- gui/src/app/SamplerOutputView/SequencePlotWidget.tsx
+++ gui/src/app/SamplerOutputView/SequencePlotWidget.tsx
@@ -22,33 +22,35 @@ const SequencePlotWidget: FunctionComponent<Props> = ({ plotSequences, variableN
 			[{type: 'rect', x0: highlightDrawRange[0], x1: highlightDrawRange[1], y0: 0, y1: 1, yref: 'paper', fillcolor: 'yellow', opacity: 0.1}]
 		) : []) as any
 	), [highlightDrawRange])
+	const data: any[] = useMemo(() => (
+		plotSequences.map(ps => (
+			{
+				x: [...new Array(ps.data.length).keys()].map(i => (i + 1)),
+				y: ps.data,
+				type: 'scatter',
+				mode: 'lines+markers',
+				marker: {color: ps.color}
+			}
+		))
+	), [plotSequences])
+	const layout = useMemo(() => ({
+		width: width,
+		height,
+		title: '',
+		yaxis: {title: variableName},
+		xaxis: {title: 'draw'},
+		shapes,
+		margin: {
+			t: 30, b: 40, r: 0
+		},
+		showlegend: false
+	}), [width, height, variableName, shapes])
 	return (
 		<div style={{ position: 'relative', width, height }}>
 			<Suspense fallback={<div>Loading plotly</div>}>
 				<Plot
-					data={
-						plotSequences.map(ps => (
-							{
-								x: [...new Array(ps.data.length).keys()].map(i => (i + 1)),
-								y: ps.data,
-								type: 'scatter',
-								mode: 'lines+markers',
-								marker: {color: ps.color}
-							}
-						))
-					}
-					layout={{
-						width: width,
-						height,
-						title: '',
-						yaxis: {title: variableName},
-						xaxis: {title: 'draw'},
-						shapes,
-						margin: {
-							t: 30, b: 40, r: 0
-						},
-						showlegend: false
-					}}
+					data={data}
+					layout={layout}
 				/>
 			</Suspense>
 		</div>
