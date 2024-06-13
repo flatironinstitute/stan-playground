c251cfb11d106783df8c24cc022b620217e4b04c
Jeremy Magland <jmagland@flatironinstitute.org>
2024-06-13 17:47:00 -0400

    import files logic

--- gui/src/app/pages/HomePage/ImportWindow.tsx
+++ gui/src/app/pages/HomePage/ImportWindow.tsx
@@ -1,4 +1,4 @@
-import { FunctionComponent, useCallback, useState } from "react"
+import { FunctionComponent, useCallback, useEffect, useState } from "react"
 import { useSPAnalysis } from "../../SPAnalysis/SPAnalysisContext"
 import JSZip from 'jszip'
 import UploadFilesArea from "./UploadFilesArea"
@@ -10,42 +10,54 @@ type ImportWindowProps = {
 const ImportWindow: FunctionComponent<ImportWindowProps> = ({ onClose }) => {
     const { localDataModel } = useSPAnalysis()
     const [errorText, setErrorText] = useState<string | null>(null)
+    const [filesUploaded, setFilesUploaded] = useState<{name: string, content: ArrayBuffer}[] | null>(null)
+    const [showReplaceProjectOptions, setShowReplaceProjectOptions] = useState<boolean>(false)
 
-    const handleFilesUploaded = useCallback(async (files: {
-        name: string,
-        content: ArrayBuffer
-    }[]) => {
+    const importUploadedFiles = useCallback(async (o: {replaceProject: boolean}) => {
+        const {replaceProject} = o
+        if (!filesUploaded) return
         try {
-            if ((files.length === 1) && (files[0].name.endsWith('.zip'))) {
-                await loadFromZip(localDataModel, files[0].name, files[0].content)
+            if ((filesUploaded.length === 1) && (filesUploaded[0].name.endsWith('.zip'))) {
+                // a single .zip file
+                await loadFromZip(localDataModel, filesUploaded[0].name, filesUploaded[0].content, {replaceProject})
             }
-            else if ((files.length === 1) && (files[0].name.endsWith('.stan'))) {
-                await loadFromStanFile(localDataModel, files[0].name, files[0].content)
+            else if ((filesUploaded.length === 1) && (filesUploaded[0].name.endsWith('.stan'))) {
+                // a single .stan file
+                await loadFromStanFile(localDataModel, filesUploaded[0].name, filesUploaded[0].content, {replaceProject})
             }
-            else if ((files.length === 1) && (files[0].name === 'data.json')) {
-                await loadFromFiles(localDataModel, files)
+            else if ((filesUploaded.length === 1) && (filesUploaded[0].name === 'data.json')) {
+                // a single data.json file
+                await loadFromFiles(localDataModel, filesUploaded, {replaceProject})
             }
             else {
-                for (const file of files) {
-                    let ok = false
-                    for (const fname of ['main.stan', 'data.json', 'sampling_opts.json', 'meta.json']) {
-                        if (file.name === fname) {
-                            ok = true
-                            break
-                        }
-                    }
-                    if (!ok) {
+                for (const file of filesUploaded) {
+                    if (!['main.stan', 'data.json', 'sampling_opts.json', 'meta.json'].includes(file.name)) {
                         throw Error('Unrecognized file: ' + file.name)
                     }
                 }
-                await loadFromFiles(localDataModel, files)
+                await loadFromFiles(localDataModel, filesUploaded, {replaceProject})
             }
             onClose()
         }
         catch (e: any) {
             setErrorText(e.message)
         }
-    }, [localDataModel, onClose])
+    }, [filesUploaded, localDataModel, onClose])
+
+    useEffect(() => {
+        if (!filesUploaded) return
+        if ((filesUploaded.length === 1) && (!filesUploaded[0].name.endsWith('.zip'))) {
+            // The user has uploaded a single file and it is not a zip file. In
+            // this case we want to give the user the option whether or not to
+            // replace the current project.
+            setShowReplaceProjectOptions(true)
+        }
+        else {
+            // Otherwise, we just go ahead and import the files, replacing the
+            // entire project
+            importUploadedFiles({replaceProject: true})
+        }
+    }, [filesUploaded, importUploadedFiles])
 
     return (
         <div>
@@ -62,12 +74,31 @@ const ImportWindow: FunctionComponent<ImportWindowProps> = ({ onClose }) => {
             <div style={{color: 'red'}}>
                 {errorText}
             </div>
-            <div>
-                <UploadFilesArea
-                    height={300}
-                    onUpload={handleFilesUploaded}
-                />
-            </div>
+            {!filesUploaded ? (
+                <div>
+                    <UploadFilesArea
+                        height={300}
+                        onUpload={setFilesUploaded}
+                    />
+                </div>
+            ) : (
+                <div>
+                    {filesUploaded.map(file => (
+                        <div key={file.name}>
+                            {file.name}
+                        </div>
+                    ))}
+                </div>
+            )}
+            {
+                showReplaceProjectOptions && (
+                    <div>
+                        <button onClick={() => importUploadedFiles({replaceProject: true})}>Import into a NEW project</button>
+                        &nbsp;
+                        <button onClick={() => importUploadedFiles({replaceProject: false})}>Import into EXISTING project</button>
+                    </div>
+                )
+            }
         </div>
     )
 }
@@ -75,27 +106,37 @@ const ImportWindow: FunctionComponent<ImportWindowProps> = ({ onClose }) => {
 const loadFromFiles = async (localDataModel: any, files: {
     name: string,
     content: ArrayBuffer
-}[]) => {
+}[], o: {replaceProject: boolean}) => {
     const stanFileContent = files.find(file => file.name === 'main.stan')?.content
     const dataFileContent = files.find(file => file.name === 'data.json')?.content
     const samplingOptsContent = files.find(file => file.name === 'sampling_opts.json')?.content
     const metaContent = files.find(file => file.name === 'meta.json')?.content
 
-    localDataModel.setStanFileContent(stanFileContent ? new TextDecoder().decode(stanFileContent) : '')
-    localDataModel.setDataFileContent(dataFileContent ? new TextDecoder().decode(dataFileContent) : '')
-    localDataModel.setSamplingOptsContent(samplingOptsContent ? new TextDecoder().decode(samplingOptsContent) : '')
-    const meta = metaContent ? JSON.parse(new TextDecoder().decode(metaContent)) : {}
-    localDataModel.setTitle(meta.title || 'Untitled')
+    if (stanFileContent || o.replaceProject) {
+        localDataModel.setStanFileContent(stanFileContent ? new TextDecoder().decode(stanFileContent) : '')
+    }
+    if (dataFileContent || o.replaceProject) {
+        localDataModel.setDataFileContent(dataFileContent ? new TextDecoder().decode(dataFileContent) : '')
+    }
+    if (samplingOptsContent || o.replaceProject) {
+        localDataModel.setSamplingOptsContent(samplingOptsContent ? new TextDecoder().decode(samplingOptsContent) : '')
+    }
+    if (metaContent || o.replaceProject) {
+        const meta = metaContent ? JSON.parse(new TextDecoder().decode(metaContent)) : {}
+        localDataModel.setTitle(meta.title || 'Untitled')
+    }
 }
 
-const loadFromStanFile = async (localDataModel: any, name: string, content: ArrayBuffer) => {
-    localDataModel.setStanFileContent(new TextDecoder().decode(content))
-    localDataModel.setDataFileContent('')
-    localDataModel.setSamplingOptsContent('')
-    localDataModel.setTitle(name)
+const loadFromStanFile = async (localDataModel: any, name: string, content: ArrayBuffer, o: {replaceProject: boolean}) => {
+    await loadFromFiles(localDataModel, [
+        {name: 'main.stan', content},
+    ], {replaceProject: o.replaceProject})
+    if (o.replaceProject) {
+        localDataModel.setTitle(name)
+    }
 }
 
-const loadFromZip = async (localDataModel: any, name: string, content: ArrayBuffer) => {
+const loadFromZip = async (localDataModel: any, _name: string, content: ArrayBuffer, o: {replaceProject: boolean}) => {
     const zip = await JSZip.loadAsync(content)
 
     // check for a single folder
@@ -118,28 +159,28 @@ const loadFromZip = async (localDataModel: any, name: string, content: ArrayBuff
             }
         }
     }
-    localDataModel.setTitle(folderName || 'Untitled') // use this if there is no meta.json
+    const files: {name: string, content: ArrayBuffer}[] = []
     for (const name in zip.files) {
         const f = zip.files[name]
         if (!f.dir) {
             if (f.name === `${(folderName || '')}main.stan`) {
-                localDataModel.setStanFileContent(await f.async('text'))
+                files.push({name: 'main.stan', content: await f.async('arraybuffer')})
             }
             else if (f.name === `${(folderName || '')}data.json`) {
-                localDataModel.setDataFileContent(await f.async('text'))
+                files.push({name: 'data.json', content: await f.async('arraybuffer')})
             }
             else if (f.name === `${(folderName || '')}sampling_opts.json`) {
-                localDataModel.setSamplingOptsContent(await f.async('text'))
+                files.push({name: 'sampling_opts.json', content: await f.async('arraybuffer')})
             }
             else if (f.name === `${(folderName || '')}meta.json`) {
-                const meta = JSON.parse(await f.async('text'))
-                localDataModel.setTitle(meta.title || 'Untitled')
+                files.push({name: 'meta.json', content: await f.async('arraybuffer')})
             }
             else {
                 throw new Error(`Unrecognized file in zip: ${f.name}`)
             }
         }
     }
+    await loadFromFiles(localDataModel, files, {replaceProject: o.replaceProject})
 }
 
 export default ImportWindow
