a5c5a6f7f829b33f7c3818a25cbcb65ecd68cbdc
Jeremy Magland <jmagland@flatironinstitute.org>
2024-06-27 12:25:30 -0400

    Add comments to loadFilesFromGist

--- gui/src/app/SPAnalysis/loadFilesFromGist.ts
+++ gui/src/app/SPAnalysis/loadFilesFromGist.ts
@@ -19,13 +19,19 @@ const loadFilesFromGist = async (gistUri: string): Promise<{ files: { [key: stri
         if (!file) continue
         let content = file.content
         if (content === undefined) continue
-        // gists do not support empty files or whitespace-only files
+        // Gists do not support empty files or whitespace-only files. This
+        // provides a workaround for that by allowing the user to specify an
+        // empty file by starting the content with the string '<<empty>>'. This
+        // is then removed from the content.
         if (content.startsWith('<<empty>>')) {
             const x = content.slice('<<empty>>'.length)
             if (x.trim() === '') {
                 content = x
             }
         }
+        // Gists do not support directories This is a placeholder for the future
+        // where we may want a subdirectory structure for project files. Here we
+        // replace the '|' character with '/' to simulate a directory structure.
         const fname2 = replaceBarsWithSlashes(fname)
         files[fname2] = content
     }
