e3680bcb954aae2dc100f5545301343fac2de653
Brian Ward <bward@flatironinstitute.org>
2024-09-05 15:26:53 +0000

    Fix comment formatting

--- gui/src/app/util/isMonacoWorkerNoise.ts
+++ gui/src/app/util/isMonacoWorkerNoise.ts
@@ -1,8 +1,7 @@
-// monaco-editor has a call to globalThis.postMessage with the '*' target
-// so our workers will recieve this message. We ignore it to prevent
-
 import baseObjectCheck from "./baseObjectCheck";
 
+// monaco-editor has a call to globalThis.postMessage with the '*' target
+// so our workers will recieve this message. We ignore it to prevent
 // anything weird from happening.
 export const isMonacoWorkerNoise = (obj: any): boolean =>
   !baseObjectCheck(obj) ||
