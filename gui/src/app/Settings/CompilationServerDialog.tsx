0322f75941d54092d967b5248c1d3fa513219e13
Brian Ward <bward@flatironinstitute.org>
2025-01-23 19:51:17 +0000

    Rework form design for settings

--- gui/src/app/Settings/CompilationServerDialog.tsx
+++ gui/src/app/Settings/CompilationServerDialog.tsx
@@ -19,6 +19,7 @@ import {
   UserSettingsContext,
 } from "@SpSettings/UserSettings";
 import { CompileContext } from "../Compile/CompileContextProvider";
+import { FormHelperText } from "@mui/material";
 
 type ConfigureCompilationServerDialogProps = {
   // none
@@ -50,10 +51,65 @@ const ConfigureCompilationServerDialog: FunctionComponent<
 
   return (
     <div className="dialogWrapper">
-      <p>
-        While the sampling is performed locally in the browser, a compilation
-        server is required to compile the Stan programs.
-      </p>
+      <FormControl>
+        <FormLabel id="compilation-server-selection">
+          <h3>Compilation server</h3>
+        </FormLabel>
+
+        <p>
+          While the sampling is performed locally in the browser, a compilation
+          server is required to compile the Stan programs.
+        </p>
+
+        <RadioGroup value={serverType} onChange={makeChoice}>
+          <FormControlLabel
+            value="public"
+            control={<Radio />}
+            label="Public server"
+          />
+          {serverType === "public" && (
+            <FormHelperText>
+              The public server{" "}
+              <span className="details">({publicCompilationServerUrl})</span> is
+              provided for convenience, but may not be as reliable as a local
+              server, depending on the current load and availability.
+            </FormHelperText>
+          )}
+          <FormControlLabel
+            value="local"
+            control={<Radio />}
+            label="Local server"
+          />
+          {serverType === "local" && (
+            <FormHelperText>
+              To start a local compilation server{" "}
+              <span className="details">({localCompilationServerUrl})</span>:
+              <pre className="dockerRun">
+                docker run -p 8083:8080 -it
+                ghcr.io/flatironinstitute/stan-wasm-server:latest
+              </pre>
+            </FormHelperText>
+          )}
+          <FormControlLabel
+            value="custom"
+            control={<Radio />}
+            label="Custom server"
+          />
+        </RadioGroup>
+        {serverType === "custom" && (
+          <div>
+            <TextField
+              variant="standard"
+              label="Custom server URL"
+              disabled={serverType !== "custom"}
+              value={stanWasmServerUrl}
+              onChange={(e) => setStanWasmServerUrl(e.target.value)}
+            />
+            <br />
+            <br />
+          </div>
+        )}
+      </FormControl>
       <Divider />
       <p>
         {isConnected ? (
@@ -74,69 +130,6 @@ const ConfigureCompilationServerDialog: FunctionComponent<
           <Refresh fontSize="inherit" />
         </IconButton>
       </p>
-      <Divider />
-
-      <FormControl>
-        <FormLabel id="compilation-server-selection">
-          Compilation server
-        </FormLabel>
-        <RadioGroup value={serverType} onChange={makeChoice}>
-          <FormControlLabel
-            value="public"
-            control={<Radio />}
-            label="Public server"
-          />
-          <FormControlLabel
-            value="local"
-            control={<Radio />}
-            label="Local server"
-          />
-          <FormControlLabel
-            value="custom"
-            control={<Radio />}
-            label="Custom server"
-          />
-        </RadioGroup>
-
-        {serverType === "custom" && (
-          <div>
-            <TextField
-              variant="standard"
-              label="Custom server URL"
-              disabled={serverType !== "custom"}
-              value={stanWasmServerUrl}
-              onChange={(e) => setStanWasmServerUrl(e.target.value)}
-            />
-            <br />
-            <br />
-          </div>
-        )}
-      </FormControl>
-
-      {serverType === "local" && (
-        <div>
-          <p>
-            To start a local compilation server{" "}
-            <span className="details">({localCompilationServerUrl})</span>:
-          </p>
-          <div>
-            <pre>
-              docker run -p 8083:8080 -it
-              ghcr.io/flatironinstitute/stan-wasm-server:latest
-            </pre>
-          </div>
-        </div>
-      )}
-      {serverType === "public" && (
-        <div>
-          <p>
-            The public server{" "}
-            <span className="details">({publicCompilationServerUrl})</span> is
-            provided for convenience, but may not be as reliable as a local
-            server, depending on the current load and availability.
-          </p>
-        </div>
-      )}
     </div>
   );
 };
--- gui/src/app/Settings/PersonalSettingsDialogue.tsx
+++ gui/src/app/Settings/PersonalSettingsDialogue.tsx
@@ -1,7 +1,9 @@
-import { useTheme } from "@mui/material/styles";
-import { Brightness7, DarkMode } from "@mui/icons-material";
 import Button from "@mui/material/Button";
 import Switch from "@mui/material/Switch";
+import FormControl from "@mui/material/FormControl";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormHelperText from "@mui/material/FormHelperText";
+import FormLabel from "@mui/material/FormLabel";
 
 import { useContext } from "react";
 
@@ -9,68 +11,82 @@ import { ProjectContext } from "@SpCore/ProjectContextProvider";
 import { UserSettingsContext } from "./UserSettings";
 
 const PersonalSettingsDialogue = () => {
-  const theme = useTheme();
-  const isLight = theme.palette.mode === "light";
-
   const { update } = useContext(ProjectContext);
-  const { pedantic, togglePedantic, toggleTheme } =
+  const { pedantic, togglePedantic, theme, toggleTheme } =
     useContext(UserSettingsContext);
 
   return (
     <div className="dialogWrapper">
-      <h3>Appearance</h3>
-      <Button
-        title="Toggle light/dark"
-        variant="contained"
-        onClick={toggleTheme}
-        startIcon={
-          isLight ? (
-            <DarkMode fontSize="inherit" />
-          ) : (
-            <Brightness7 fontSize="inherit" />
-          )
-        }
-      >
-        Toggle {isLight ? "Dark" : "Light"} mode
-      </Button>
-      <h3>Warnings</h3>
-      <p>
-        &quot;Pedantic Mode&quot; is an option in the Stan compiler to enable
-        additional warnings. These can be instructive, but they can also lead to
-        false positives. If the compiler is giving you a warning that you think
-      </p>
+      <FormControl>
+        <FormLabel id="appearance-settings">
+          <h3>Appearance</h3>
+        </FormLabel>
+        <FormControlLabel
+          value="public"
+          control={<Switch onClick={toggleTheme} checked={theme == "dark"} />}
+          label="Enable dark mode"
+          title="Enable dark mode"
+          aria-label="enable dark mode"
+        />
+        <FormHelperText>
+          Dark mode makes the background of the app dark and changes other
+          colors to be easier on the eyes. It does <em>not</em> affect items
+          like the plots produced by the analysis scripts.
+        </FormHelperText>
+      </FormControl>
 
-      <Switch
-        checked={pedantic}
-        onChange={togglePedantic}
-        color="primary"
-        name="pedantic"
-        aria-label="toggle pedantic mode"
-        title="Toggle pedantic mode"
-      />
+      <FormControl>
+        <FormLabel id="warnings-settings">
+          <h3>Warnings</h3>
+        </FormLabel>
+        <FormControlLabel
+          value="public"
+          control={<Switch onClick={togglePedantic} checked={pedantic} />}
+          label="Enable Pedantic mode"
+          aria-label="enable pedantic mode"
+          title="Enable Pedantic mode"
+        />
+        <FormHelperText>
+          <a
+            href="https://mc-stan.org/docs/stan-users-guide/using-stanc.html#pedantic-mode"
+            target="_blank"
+            rel="noreferrer"
+          >
+            &quot;Pedantic mode&quot;
+          </a>{" "}
+          is an option in the Stan compiler to enable additional warnings. These
+          can be instructive, but they can also lead to false positives. Be
+          prepared to take them with a grain of salt.
+        </FormHelperText>
+      </FormControl>
 
-      <h3> Reset to defaults</h3>
-      <p>
-        Reset all settings to their default values and clear the current model.
-      </p>
-      <p>Make sure to save your work first!</p>
-      <Button
-        variant="outlined"
-        color="error"
-        onClick={() => {
-          const ok = window.confirm(
-            "Are you sure you want to clear ALL DATA (including the editors)?",
-          );
-          if (!ok) return;
-          update({ type: "clear" });
-          localStorage.clear();
-          setTimeout(() => {
-            window.location.reload();
-          }, 25);
-        }}
-      >
-        Clear all data
-      </Button>
+      <FormControl>
+        <FormLabel id="reset-settings">
+          <h3>Reset to defaults</h3>
+        </FormLabel>
+        <Button
+          variant="outlined"
+          color="error"
+          onClick={() => {
+            const ok = window.confirm(
+              "Are you sure you want to clear ALL DATA (including the editors)?",
+            );
+            if (!ok) return;
+            update({ type: "clear" });
+            localStorage.clear();
+            setTimeout(() => {
+              window.location.reload();
+            }, 25);
+          }}
+        >
+          Clear all data
+        </Button>
+        <FormHelperText>
+          Reset all settings to their default values and clear the current
+          model. <br />
+          Make sure to download or export your work first!
+        </FormHelperText>
+      </FormControl>
     </div>
   );
 };
