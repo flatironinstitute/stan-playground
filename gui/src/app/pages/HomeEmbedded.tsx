60fbe4379f34f42ba82dbef908059f3d34e55aa1
Jeremy Magland <jmagland@flatironinstitute.org>
2025-09-23 18:45:15 -0400

    in embedded view, show sampler status in output tab

--- gui/src/app/pages/HomeEmbedded.tsx
+++ gui/src/app/pages/HomeEmbedded.tsx
@@ -1,16 +1,20 @@
 import SamplerOutputArea from "@SpAreas/ControlArea/SamplingArea/ResultsArea/SamplerOutputArea";
+import SamplingProgressComponent from "@SpAreas/ControlArea/SamplingArea/RunArea/SamplerProgress";
+import SamplerStatusPanel from "@SpAreas/ControlArea/SamplingArea/RunArea/SamplerStatusPanel";
 import EmbeddedBottomBar from "@SpAreas/EmbeddedBottomBar";
 import DataEditorPanel from "@SpAreas/ModelDataArea/DataEditorPanel";
 import ModelEditorPanel from "@SpAreas/ModelDataArea/ModelEditorPanel";
 import TabWidget from "@SpComponents/TabWidget";
 import { CompileContext } from "@SpCore/Compilation/CompileContextProvider";
+import { ProjectContext } from "@SpCore/Project/ProjectContextProvider";
 import useStanSampler from "@SpCore/StanSampler/useStanSampler";
-import { FunctionComponent, use, useEffect, useState } from "react";
+import { FunctionComponent, use, useEffect, useMemo, useState } from "react";
 
 const HomeEmbedded: FunctionComponent = () => {
   const { compiledMainJsUrl } = use(CompileContext);
   const { sampler, samplerState } = useStanSampler(compiledMainJsUrl);
   const [currentTab, setCurrentTab] = useState<number | undefined>(undefined);
+  const { data: projectData } = use(ProjectContext);
 
   useEffect(() => {
     if (samplerState.status === "completed") {
@@ -24,11 +28,18 @@ const HomeEmbedded: FunctionComponent = () => {
     document.title = "Stan Playground Embedded";
   }, []);
 
+  const outputLabel = useMemo(() => {
+    if (samplerState.status === "sampling") {
+      return "Output...";
+    }
+    return "Output";
+  }, [samplerState.status]);
+
   return (
     <div className="HomeEmbedded">
       <div style={{ flex: 1, minHeight: 0 }}>
         <TabWidget
-          labels={["Stan", "Data", "Output"]}
+          labels={["Stan", "Data", outputLabel]}
           forcedSelection={currentTab}
         >
           <ModelEditorPanel />
@@ -36,7 +47,10 @@ const HomeEmbedded: FunctionComponent = () => {
           {samplerState.latestRun ? (
             <SamplerOutputArea latestRun={samplerState.latestRun} />
           ) : (
-            <div>Run the sampler to see output.</div>
+            <SamplerStatusPanel
+              samplerState={samplerState}
+              numChains={projectData.samplingOpts.num_chains}
+            />
           )}
         </TabWidget>
       </div>
