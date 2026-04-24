8068db9e7c2f580b370dc71f7c057ce409306145
Brian Ward <bward@flatironinstitute.org>
2026-04-24 15:37:09 -0400

    Remove brms-shimmed variables from global environment at start of run (#357)

--- gui/src/app/core/Scripting/webR/webR_preamble.R
+++ gui/src/app/core/Scripting/webR/webR_preamble.R
@@ -2,3 +2,8 @@
 webr::shim_install()
 # enable the canvas backend for plotting
 webr::canvas()
+
+# cleanliness -- brms shim modifies persistent environment, clean that up
+if (exists(".SP_CODE")) {
+  remove(".SP_CODE", "data", envir = .GlobalEnv)
+}
